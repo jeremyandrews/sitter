6d35807cb6e0f7a96295c2690f22ae62b4c768e3
Jeremy Andrews <jeremy@tag1.com>
2020-12-14 16:34:06 +0100

    properly manage db externally

--- src/person.rs
+++ src/person.rs
@@ -4,7 +4,9 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use sqlx::postgres::PgPool;
+use anyhow::Result;
+use sqlx::{PgPool, Row};
+use sqlx::postgres::PgRow;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Person {
@@ -15,63 +17,84 @@ pub struct Person {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+pub struct PersonRequest {
+    pub name: String,
+    pub email: String,
+    pub pass: String,
+}
+
+impl Person {
+    /// A helper function to add a new Person object to the database.
+    pub async fn create(person: PersonRequest, db: &PgPool) -> Result<Person> {
+        // @TODO: validate (and/or allow validation of) name, email and pass.
+
+        let mut transaction = db.begin().await?;
+        let person = sqlx::query("INSERT INTO person (name, email, pass) VALUES ($1, $2) RETURNING id, name, email, pass")
+            .bind(&person.name)
+            .bind(&person.email)
+            .bind(&person.pass)
+            .map(|row: PgRow| {
+                Person {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                    pass: row.get(3)
+                }
+            })
+            .fetch_one(&mut transaction)
+            .await?;
+        transaction.commit().await?;
+
+        // @TODO: logging.
+
+        // @TODO: hook_create.
+
+        Ok(person)
+    }
+
+    pub async fn read(_person_filter: Option<PersonFilter>, db: &PgPool) -> Result<Vec<Person>> {
+        // @TODO build optional filter query.
+
+        let mut persons = vec![];
+
+        let records = sqlx::query(
+            r#"
+                SELECT id, name, email, pass
+                FROM person
+            "#
+        )
+            .map(|row: PgRow| {
+                Person {
+                    id: row.get(0),
+                    name: row.get(1),
+                    email: row.get(2),
+                    pass: row.get(3)
+                }
+            })
+            .fetch_all(db)
+            .await?;
+        
+        for record in records {
+            persons.push(Person {
+                id: record.id,
+                name: record.name,
+                email: record.email,
+                pass: record.pass,
+            });
+        }
+
+        // @TODO: logging.
+
+        // @TODO: hook_read.
+
+        Ok(persons)
+    }
+}
+
+
+#[derive(Deserialize, Serialize, Debug)]
 pub struct PersonFilter {
     pub ids: Option<Vec<Uuid>>,
     pub names: Option<Vec<String>>,
     pub emails: Option<Vec<String>>,
-}
-
-/// A helper function to add a new Person object to the database.
-pub async fn create(
-    db: &PgPool,
-    name: &str,
-    email: &str,
-    pass: &str
-) -> anyhow::Result<String> {
-    // @TODO: validate (and/or allow validation of) name, email and pass.
-
-    // @TODO: error handling.
-    let insert = sqlx::query!(
-        r#"
-INSERT INTO person (name, email, pass)
-VALUES ($1, $2, $3)
-RETURNING id
-        "#,
-        name,
-        email,
-        pass
-    )
-    .fetch_one(db)
-    .await?;
-
-    // @TODO: logging.
-
-    // @TODO: hook_create.
-
-    Ok(insert.id.to_string())
-}
-
-/// A helper function to add a new Person object to the database.
-pub async fn read(
-    db: &PgPool,
-    _person_filter: Option<PersonFilter>
-) -> anyhow::Result<Vec<Person>> {
-
-    // @TODO build optional filter query.
-
-    let persons = sqlx::query_as!(
-        Person,
-        r#"
-SELECT id, name, email, pass
-FROM person
-        "#
-    )
-    .fetch_all(db)
-    .await?;
-
-    // @TODO: logging.
-
-    // @TODO: hook_read.
-
-    Ok(persons)
 }
